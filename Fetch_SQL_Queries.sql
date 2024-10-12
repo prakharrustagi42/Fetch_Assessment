953366373a95d398730c4e025067b28dbab8c245
Prakhar Rustagi <prakharrustagi42@gmail.com>
2024-10-12 12:10:42 -0500

    Update Fetch_SQL_Queries.sql

--- Fetch_SQL_Queries.sql
+++ Fetch_SQL_Queries.sql
@@ -1,7 +1,7 @@
 -- Go Through the Ipynb file to understand the Transformation done on data . ( Receipts table was exploded using RewardsItemListColumn to get brand information)
 -- With the data model taken we can answer more than 2 questions just by using a window function and then taking the
 --row number 1. As columns like the total spent , purchase item quantity is going to remain the same. 
-
+-- MYSQL was used for the Queries
 
 
 -- Q1) When considering average spend from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
